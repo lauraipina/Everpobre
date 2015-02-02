95bcdac3d03ace3c198a3528600f4c96988129a1
Laura Iglesias <laura.iglesias.pina@gmail.com>
2015-02-02 12:05:16 +0100

    Initial Commit

---
+++ Everpobre/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Everpobre
+//
+//  Created by Laura Iglesias Piña on 2/2/15.
+//  Copyright (c) 2015 lip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
